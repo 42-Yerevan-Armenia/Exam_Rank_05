e2718fb61eb5ac3df5658e8bd087b9c967666827
Aramxxx8691 <khurshudyan2013@gmail.com>
2023-08-18 00:47:00 -0400

    cpp_00

---
+++ cpp_module_00/Warlock.cpp
@@ -0,0 +1,22 @@
+#include "Warlock.hpp"
+
+//Warlock::Warlock();
+Warlock::~Warlock() {std::cout << _name << ": My job here is done!" << std::endl;}
+Warlock::Warlock(std::string name, std::string title) : _name(name), _title(title) 
+{std::cout << _name << ": This looks like another boring day." << std::endl;}
+
+std::string const &Warlock::getName() const {return (_name);}
+std::string const &Warlock::getTitle() const {return (_title);}
+
+void	Warlock::setTitle(std::string const &str) {_title = str;}
+void	Warlock::introduce() const {std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;}
+
+/*
+Warlock::Warlock(Warlock const &copy) {*this = copy;};
+Warlock &Warlock::operator=(Warlock const &other)
+{
+	_name = other._name;
+	_title = other._title;
+	return *this;
+}
+*/
