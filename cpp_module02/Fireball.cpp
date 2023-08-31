b9634f28af7be4c7159807f5bc08f05526770227
Aramxxx8691 <khurshudyan2013@gmail.com>
2023-08-31 21:08:34 +0400

    fixed

--- cpp_module02/Fireball.cpp
+++ cpp_module02/Fireball.cpp
@@ -1,5 +1,5 @@
 #include "Fireball.hpp"
 
-Fireball::Fireball() : ASpell("Fireball", "burnt to a crisp!") {}
+Fireball::Fireball() : ASpell("Fireball", "burnt to a crisp") {}
 Fireball::~Fireball(){}
 ASpell *Fireball::clone() const {return (new Fireball());}
