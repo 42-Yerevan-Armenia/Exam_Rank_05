f940ef4f0211a3a45a230f06ac8312172381ea3b
Aramxxx8691 <khurshudyan2013@gmail.com>
2023-09-07 15:52:39 +0400

    done

--- cpp_module01/ATarget.cpp
+++ cpp_module01/ATarget.cpp
@@ -6,7 +6,7 @@ ATarget::ATarget(std::string const &type) : _type(type){}
 std::string const &ATarget::getType() const {return (_type);}
 
 void    ATarget::getHitBySpell(ASpell const &spell) const
-{std::cout << _type << " has been " << spell.getEffects() << std::endl;}
+{std::cout << _type << " has been " << spell.getEffects() << "!" << std::endl;}
 
 // ATarget::ATarget(){};
 // ATarget::ATarget(ATarget const &copy) {*this = copy;};
